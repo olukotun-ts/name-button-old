40744804916b05bcfaadd9fc88700d6163973dc9
Sam O <olukotun.ts@gmail.com>
2019-01-23 00:24:28 -0800

    Render button

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/static', express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname + '/public/' });
+});
 app.listen(port, () => console.log(`Listening on port ${port}.`));
